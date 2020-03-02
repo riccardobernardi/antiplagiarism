19507a8e901544f194b6f839378ffce2b8b887bb
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-03-02 13:46:30 +0100

    improved

--- antiplagiarism/main.py
+++ antiplagiarism/main.py
@@ -3,6 +3,8 @@ from antiplagiarism.util import antiplagiarism
 
 def main():
 	antiplagiarism()
+	print("----------------")
+	antiplagiarism(grams=10)
 
 if __name__ == "__main__":
 	main()
--- antiplagiarism/util.py
+++ antiplagiarism/util.py
@@ -4,13 +4,20 @@ import hashlib
 
 files = dict({})
 
-
-def helper(x):
+def helper(x, grams):
 	with open(x, "r") as ff:
 		files[x] = set()
+		count = 0
+		s = ""
 		for i in ff:
-			hash_object = hashlib.md5(i.encode("utf-8"))
-			files[x].add(hash_object.hexdigest())
+			files[x].add(hashlib.md5(i.encode("utf-8")).hexdigest())
+			s += i
+			count += 1
+			if count == grams:
+				files[x].add(hashlib.md5(s.encode("utf-8")).hexdigest())
+				s = list(s.split("\n")[1:]).reduce(lambda x,y: IF(x=="", x+y,x+"\n"+y ))
+				count-=1
+
 
 def jaccard(x, u):
 	try:
@@ -19,10 +26,12 @@ def jaccard(x, u):
 		return 0
 
 
-def antiplagiarism(s="", type = ".c"):
-	if s=="":
-		list(os.listdir("./")).filter(lambda x: type in x).map(lambda x: helper(x))
+def antiplagiarism(s="", type=".c", grams=2):
+	if s == "":
+		list(os.listdir("./")).filter(lambda x: type in x).map(lambda x: helper(x, grams))
 	else:
-		list(os.listdir(s)).filter(lambda x: type in x).map(lambda x: helper(x))
+		list(os.listdir(s)).filter(lambda x: type in x).map(lambda x: helper(x, grams))
 	exited = set()
-	files.map(lambda x: files.filter(lambda m: m[0]!=x[0]).next(lambda k: exited.add(x[0])).filter(lambda f: f[0] not in exited).map(lambda u: (x[0], u[0],jaccard(x,u))).map(lambda x: x[0] + " == " + x[1] + " --> " + str(round(x[2]*100,2)) + "%").next(lambda x: print(x)))
+	files.map(lambda x: files.filter(lambda m: m[0] != x[0]).next(lambda k: exited.add(x[0])).filter(
+		lambda f: f[0] not in exited).map(lambda u: (x[0], u[0], jaccard(x, u))).map(
+		lambda x: x[0] + " == " + x[1] + " --> " + str(round(x[2] * 100, 2)) + "%").next(lambda x: print(x)))
