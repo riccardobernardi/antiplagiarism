2faaac877d2eaedde082e88257a36e2fc3d1c455
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-03-07 16:40:51 +0100

    improved

--- antiplagiarism/util.py
+++ antiplagiarism/util.py
@@ -5,16 +5,22 @@ import hashlib
 files = dict({})
 
 def helper(x, grams):
-	with open(x, "r") as ff:
+	with open(x, "r", encoding='utf-8') as ff:
 		files[x] = set()
 		count = 0
 		s = ""
 		for i in ff:
-			files[x].add(hashlib.md5(i.encode("utf-8")).hexdigest())
+			try:
+				files[x].add(hashlib.md5(i.encode('utf-8')).hexdigest())
+			except:
+				print("error of codec in: ", x)
 			s += i
 			count += 1
 			if count == grams:
-				files[x].add(hashlib.md5(s.encode("utf-8")).hexdigest())
+				try:
+					files[x].add(hashlib.md5(s.encode("utf-8")).hexdigest())
+				except:
+					print("error of codec in: ", x)
 				s = list(s.split("\n")[1:]).reduce(lambda x,y: IF(x=="", x+y,x+"\n"+y ))
 				count-=1
 
@@ -30,8 +36,19 @@ def antiplagiarism(path="", type=".c", grams=2):
 	if path == "":
 		list(os.listdir("./")).filter(lambda x: type in x).map(lambda x: helper(x, grams))
 	else:
-		list(os.listdir(path)).filter(lambda x: type in x).map(lambda x: helper(x, grams))
+		list(os.listdir(path)).filter(lambda x: type in x).map(lambda x: helper(path + "/" + x, grams))
 	exited = set()
+	# maxim = set()
 	files.map(lambda x: files.filter(lambda m: m[0] != x[0]).next(lambda k: exited.add(x[0])).filter(
 		lambda f: f[0] not in exited).map(lambda u: (x[0], u[0], jaccard(x, u))).map(
-		lambda x: x[0] + " == " + x[1] + " --> " + str(round(x[2] * 100, 2)) + "%").next(lambda x: print(x)))
+		lambda x: x[0].split("/")[-1] + " == " + x[1].split("/")[-1] + " --> " + str(round(x[2] * 100, 2)) + "%"))
+	# .next(lambda x: print(x)).next(
+	# 	maxim.add([round(x[2] * 100, 2), str(x[0] + " == " + x[1])])
+	# ))
+	#
+	# maxim = list(maxim)
+	# sorted(maxim, key=lambda x: x[1])
+
+	#print()
+
+	#.next(lambda x: print(x)))
